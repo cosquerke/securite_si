cf92ba2021452d0ef5a92f58f14a8009f8b3192b
Kevin Cosquer <kcosquer56@gmail.com>
2023-12-15 12:21:25 +0100

    Fichier initialisation + scénario chef de pole

---
+++ generate/init_bdd.sql
@@ -0,0 +1,299 @@
+PROMPT *** Création des tables
+CREATE TABLE poles (
+	id_pole NUMBER PRIMARY KEY,
+	nom_pole VARCHAR2(50),
+	id_employe NUMBER
+);
+
+CREATE TABLE employes (
+	id_employe NUMBER PRIMARY KEY,
+	nom_employe VARCHAR2(100),
+	id_pole NUMBER,
+	salaire NUMBER
+);
+
+CREATE TABLE projets (
+	id_projet NUMBER PRIMARY KEY,
+	nom_projet VARCHAR2(100),
+	id_chef_de_projet NUMBER,
+	id_pole NUMBER
+);
+
+CREATE TABLE taches (
+	id_tache NUMBER PRIMARY KEY,
+	nom_tache VARCHAR2(100),
+	id_responsable_tache NUMBER,
+	etat_tache VARCHAR2(100),
+	id_projet NUMBER
+);
+
+PROMPT *** Création des clés étrangères
+ALTER TABLE poles ADD CONSTRAINT fk_id_employe FOREIGN KEY (id_employe) REFERENCES employes(id_employe);
+ALTER TABLE projets ADD CONSTRAINT fk_projets_id_pole FOREIGN KEY (id_pole) REFERENCES poles(id_pole);
+ALTER TABLE projets ADD CONSTRAINT fk_projets_id_chef_projet FOREIGN KEY (id_chef_de_projet) REFERENCES employes(id_employe);
+ALTER TABLE taches ADD CONSTRAINT fk_taches_id_projet FOREIGN KEY (id_projet) REFERENCES projets(id_projet);
+ALTER TABLE taches ADD CONSTRAINT fk_taches_id_responsable_tache FOREIGN KEY (id_responsable_tache) REFERENCES employes(id_employe);
+
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (101, 'Jean Dupont', 1, 50000);
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (102, 'Alice Tremblay', 2, 55000);
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (103, 'Éric Leclerc', 3, 48000);    
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (104, 'USER7', 2, 55000);
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (105, 'USER6', 3, 48000);
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (106, 'USER5', 2, 55000);
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (107, 'USER4', 1, 48000);
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (108, 'USER3', 2, 55000);
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (109, 'USER2', 1, 48000);
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (110, 'USER1', 3, 48000);
+
+
+INSERT INTO poles (id_pole, nom_pole, id_employe) VALUES (1, 'Pôle Marketing', 101);
+INSERT INTO poles (id_pole, nom_pole, id_employe) VALUES (2, 'Pôle R_D', 102);
+INSERT INTO poles (id_pole, nom_pole, id_employe) VALUES (3, 'Pôle Ventes', 103);
+INSERT INTO poles (id_pole, nom_pole, id_employe) VALUES (4, 'Pôle DSI', 107);
+
+INSERT INTO projets (id_projet, nom_projet, id_chef_de_projet, id_pole) VALUES (501, 'Projet A', '102', 1);
+INSERT INTO projets (id_projet, nom_projet, id_chef_de_projet, id_pole) VALUES (502, 'Projet B', '101', 2);
+INSERT INTO projets (id_projet, nom_projet, id_chef_de_projet, id_pole) VALUES (503, 'Projet C', '103', 3);
+INSERT INTO projets (id_projet, nom_projet, id_chef_de_projet, id_pole) VALUES (504, 'Projet D', '109', 3);
+
+INSERT INTO taches (id_tache, nom_tache, id_responsable_tache, etat_tache, id_projet) VALUES (1004, 'Tâche 4', 105, 'En cours', 504);
+INSERT INTO taches (id_tache, nom_tache, id_responsable_tache, etat_tache, id_projet) VALUES (1001, 'Tâche 1', 101, 'En cours', 501);
+INSERT INTO taches (id_tache, nom_tache, id_responsable_tache, etat_tache, id_projet) VALUES (1002, 'Tâche 2', 102, 'En attente', 502);
+INSERT INTO taches (id_tache, nom_tache, id_responsable_tache, etat_tache, id_projet) VALUES (1003, 'Tâche 3', 103, 'Terminée', 503);
+
+CREATE ROLE ADMIN10_CHEF_PROJET;
+CREATE ROLE ADMIN10_CHEF_POLE;
+CREATE ROLE ADMIN10_EMPLOYE;
+CREATE ROLE ADMIN10_RH;
+CREATE ROLE ADMIN10_ADMIN;
+
+
+GRANT SELECT,UPDATE,DELETE, INSERT ON taches TO ADMIN10_CHEF_PROJET;
+GRANT SELECT ON poles TO ADMIN10_CHEF_PROJET;
+GRANT SELECT,UPDATE,insert ON projets  TO ADMIN10_CHEF_PROJET;
+GRANT SELECT ON employes TO ADMIN10_CHEF_PROJET;
+
+
+
+GRANT SELECT,UPDATE,INSERT  ON taches TO ADMIN10_CHEF_POLE;
+GRANT SELECT ON poles TO ADMIN10_CHEF_POLE;
+GRANT SELECT ON projets  TO ADMIN10_CHEF_POLE;
+GRANT SELECT ON employes TO ADMIN10_CHEF_POLE;
+
+
+GRANT SELECT,UPDATE ON taches TO ADMIN10_EMPLOYE;
+GRANT SELECT ON poles TO  ADMIN10_EMPLOYE;
+GRANT SELECT  ON projets  TO ADMIN10_EMPLOYE;
+GRANT SELECT ON employes TO ADMIN10_EMPLOYE;
+
+GRANT SELECT,UPDATE,INSERT ON employes TO ADMIN10_RH;
+GRANT SELECT ON poles TO ADMIN10_RH;
+GRANT SELECT ON projets TO ADMIN10_RH;
+
+GRANT SELECT,UPDATE, INSERT, DELETE ON poles TO ADMIN10_ADMIN;
+
+CREATE OR REPLACE CONTEXT entreprise_ctx USING set_entreprise_ctx_pkg;
+CREATE OR REPLACE PACKAGE set_entreprise_ctx_pkg IS
+    PROCEDURE set_role;
+END;
+/
+
+CREATE OR REPLACE PACKAGE BODY set_entreprise_ctx_pkg IS
+    PROCEDURE set_role IS
+   	 role VARCHAR2(20);
+   	 id_employe_session NUMBER;
+	 id_pole_session NUMBER;
+	BEGIN
+
+	-- Requête permettant de récupérer le rôle de l'utilisateur qui est connecté
+   	 SELECT granted_role INTO role
+   	 FROM DBA_ROLE_PRIVS
+   	 WHERE granted_role like 'ADMIN10%' AND GRANTEE=user
+   	 AND ROWNUM=1;
+	 
+     SELECT id_employe INTO id_employe_session
+     FROM ADMIN10.EMPLOYES
+     WHERE nom_employe=user;
+	 
+	 SELECT id_pole INTO id_pole_session
+     FROM ADMIN10.EMPLOYES
+     WHERE nom_employe=user;
+    -- Initialisation des variables de session
+   	 DBMS_SESSION.SET_CONTEXT('entreprise_ctx', 'role_nom', role);
+     DBMS_SESSION.SET_CONTEXT('entreprise_ctx', 'id_employe',id_employe_session);
+	 DBMS_SESSION.SET_CONTEXT('entreprise_ctx', 'id_pole',id_pole_session);
+    -- Si pas de données alors NULL
+   	 EXCEPTION
+   		 WHEN NO_DATA_FOUND THEN NULL;
+
+ 	END set_role;
+END set_entreprise_ctx_pkg;
+/
+SHOW ERROR;
+
+
+
+GRANT EXECUTE ON set_entreprise_ctx_pkg to ADMIN10_CHEF_PROJET;
+GRANT EXECUTE ON set_entreprise_ctx_pkg to ADMIN10_CHEF_POLE;
+GRANT EXECUTE ON set_entreprise_ctx_pkg to ADMIN10_EMPLOYE;
+GRANT EXECUTE ON set_entreprise_ctx_pkg to ADMIN10_RH;
+GRANT EXECUTE ON set_entreprise_ctx_pkg to ADMIN10_ADMIN;
+
+PROMPT *** Attribution des droits aux utilisateurs
+GRANT ADMIN10_ADMIN to USER1;
+GRANT ADMIN10_CHEF_PROJET to USER2;
+GRANT ADMIN10_CHEF_POLE to USER3, USER4;
+GRANT ADMIN10_RH to USER5;
+GRANT ADMIN10_EMPLOYE to USER6, USER7
+
+CREATE OR REPLACE FUNCTION VPD_EMPLOYES(
+	SCHEMA_VAR IN VARCHAR2,
+	TABLE_VAR IN VARCHAR2)
+RETURN VARCHAR2
+IS
+	return_val VARCHAR2(400);
+	le_role VARCHAR2(40);
+
+BEGIN
+	le_role := SYS_CONTEXT('entreprise_ctx', 'role_nom');
+
+	IF le_role IN ('ADMIN10_EMPLOYE', 'ADMIN10_CHEF_PROJET', 'ADMIN10_CHEF_POLE') THEN
+ 
+    	return_val := 'id_employe = SYS_CONTEXT(''entreprise_ctx'', ''id_employe'')';
+	ELSE
+    	return_val := '1=1';
+	END IF;
+
+	RETURN return_val;
+END VPD_EMPLOYES;
+/
+
+BEGIN
+    DBMS_RLS.ADD_POLICY (
+   	 OBJECT_SCHEMA => 'ADMIN10',
+   	 OBJECT_NAME => 'EMPLOYES',
+   	 POLICY_NAME => 'EMPLOYES_POLICY',
+   	 FUNCTION_SCHEMA => 'ADMIN10',
+   	 POLICY_FUNCTION => 'VPD_EMPLOYES',
+   	 STATEMENT_TYPES => 'SELECT'
+    );
+END;
+/
+
+EXECUTE admin10.set_entreprise_ctx_pkg.set_role;
+SELECT sys_context('entreprise_ctx','role_nom') FROM DUAL;
+
+SELECT sys_context('entreprise_ctx','id_employe') FROM DUAL;
+
+
+
+CREATE OR REPLACE FUNCTION VPD_POLE(
+	SCHEMA_VAR IN VARCHAR2,
+	TABLE_VAR IN VARCHAR2)
+RETURN VARCHAR2
+IS
+	return_val VARCHAR2(400);
+	le_role VARCHAR2(40);
+
+BEGIN
+	le_role := SYS_CONTEXT('entreprise_ctx', 'role_nom');
+
+	IF le_role IN ('ADMIN10_EMPLOYE', 'ADMIN10_CHEF_PROJET', 'ADMIN10_CHEF_POLE') THEN
+ 
+    	return_val := 'id_pole = SYS_CONTEXT(''entreprise_ctx'', ''id_pole'')';
+	ELSE
+    	return_val := '1=1';
+	END IF;
+
+	RETURN return_val;
+END VPD_POLE;
+/
+
+BEGIN
+    DBMS_RLS.ADD_POLICY (
+   	 OBJECT_SCHEMA => 'ADMIN10',
+   	 OBJECT_NAME => 'POLES',
+   	 POLICY_NAME => 'POLES_POLICY',
+   	 FUNCTION_SCHEMA => 'ADMIN10',
+   	 POLICY_FUNCTION => 'VPD_POLE',
+   	 STATEMENT_TYPES => 'SELECT'
+    );
+END;
+/
+
+CREATE OR REPLACE FUNCTION VPD_TACHES (
+	schema_var IN VARCHAR2,
+	table_var IN VARCHAR2
+) RETURN VARCHAR2
+IS
+  return_val VARCHAR2(400);
+  le_role VARCHAR2(40);
+
+BEGIN
+	-- Récupération du rôle de l'utilisateur connecté depuis le contexte
+   le_role := SYS_CONTEXT('entreprise_ctx', 'role_nom');
+
+
+	-- Filtrage des tâches en fonction du rôle de l'utilisateur
+	IF le_role  = 'ADMIN10_CHEF_PROJET' THEN
+    	return_val := 'id_projet IN (SELECT id_projet FROM projets WHERE id_chef_de_projet = SYS_CONTEXT(''entreprise_ctx'', ''id_employe''))';
+	ELSIF le_role  = 'ADMIN10_EMPLOYE' THEN
+    	return_val := 'id_responsable_tache = SYS_CONTEXT(''entreprise_ctx'', ''id_employe'')';
+	ELSE
+    	return_val := '1=1';
+	END IF;
+
+	RETURN return_val ;
+END VPD_TACHES;
+/
+    
+
+-- Activer la politique de sécurité VPD pour la table TACHES
+BEGIN
+	DBMS_RLS.ADD_POLICY(
+    	OBJECT_SCHEMA	=> 'ADMIN10',    	 
+  	OBJECT_NAME 	=> 'TACHES',
+    	POLICY_NAME 	=> 'TACHES_POLICY',
+    	FUNCTION_SCHEMA  =>'ADMIN10',
+    	POLICY_FUNCTION  => 'VPD_TACHES',
+    	STATEMENT_TYPES  => 'SELECT, INSERT, UPDATE, DELETE'
+	);
+END;
+/
+
+CREATE OR REPLACE FUNCTION VPD_PROJET(
+	schema_var IN VARCHAR2,
+    TABLE_VAR IN VARCHAR2)
+RETURN VARCHAR2
+IS
+    return_val VARCHAR2(400);
+    le_role VARCHAR2(40);
+
+BEGIN
+    le_role := SYS_CONTEXT('entreprise_ctx', 'role_nom');
+
+    IF le_role = 'ADMIN10_CHEF_PROJET'
+    	THEN return_val := 'id_chef_de_projet = SYS_CONTEXT(''entreprise_ctx'', ''id_employe'')';
+	ELSIF le_role = 'ADMIN10_CHEF_POLE'
+    	THEN return_val := 'id_pole = SYS_CONTEXT(''entreprise_ctx'', ''id_pole'')';
+	ELSE return_val := '1=1';
+    END IF;
+
+	RETURN return_val;
+END VPD_PROJET;
+/
+
+BEGIN
+	DBMS_RLS.ADD_POLICY (
+  	  OBJECT_SCHEMA => 'ADMIN10',
+  	  OBJECT_NAME => 'PROJETS',
+  	  POLICY_NAME => 'PROJET_POLICY',
+  	  FUNCTION_SCHEMA => 'ADMIN10',
+  	  POLICY_FUNCTION => 'VPD_PROJET',
+  	  STATEMENT_TYPES => 'SELECT, UPDATE, INSERT'
+	);
+END;
+/
+
+
+
---
+++ scenarios/chef_pole.sql
@@ -0,0 +1,6 @@
+CONNECT USER4/user@cienetdb;
+EXECUTE admin10.set_entreprise_ctx_pkg.set_role;
+SELECT * FROM admin10.POLES;
+SELECT * FROM admin10.PROJETS;
+SELECT * FROM admin10.EMPLOYES;
+SELECT * FROM admin10.TACHES;
