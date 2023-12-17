6fc7685792dfaa55388ed859fd2b3c59ba0177cf
Arnaud L <lartiguear@gmail.com>
2023-12-17 15:43:50 +0100

    scenario RH et chef projet + enlever insert des VPD

---
+++ scenarios/RH.sql
@@ -0,0 +1,14 @@
+CONNECT USER5/user@cienetdb;
+EXECUTE admin10.set_entreprise_ctx_pkg.set_role;
+
+SELECT * FROM admin10.TACHES;
+
+SELECT * FROM admin10.POLES;
+
+SELECT * FROM admin10.PROJETS;
+
+SELECT * FROM admin10.EMPLOYES;
+UPDATE admin10.employes SET salaire = 50000;
+INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (111, 'USER100', 3, 100000);
+SELECT * FROM admin10.EMPLOYES;
+DELETE FROM admin10.employes WHERE id_employe = 111;
