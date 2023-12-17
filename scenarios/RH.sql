83c59e2803c73b960b2649969e333c59ff329d63
Arnaud L <lartiguear@gmail.com>
2023-12-17 15:54:25 +0100

    ajout admin10

--- scenarios/RH.sql
+++ scenarios/RH.sql
@@ -9,6 +9,6 @@ SELECT * FROM admin10.PROJETS;
 
 SELECT * FROM admin10.EMPLOYES;
 UPDATE admin10.employes SET salaire = 50000;
-INSERT INTO employes (id_employe, nom_employe, id_pole, salaire) VALUES (111, 'USER100', 3, 100000);
+INSERT INTO admin10.employes (id_employe, nom_employe, id_pole, salaire) VALUES (111, 'USER100', 3, 100000);
 SELECT * FROM admin10.EMPLOYES;
 DELETE FROM admin10.employes WHERE id_employe = 111;
--- scenarios/chef_projet.sql
+++ scenarios/chef_projet.sql
@@ -7,12 +7,12 @@ SELECT * FROM admin10.EMPLOYES;
 
 SELECT * FROM admin10.PROJETS;
 UPDATE admin10.projets SET nom_projet = "projet_correction";
-INSERT INTO projets (id_projet, nom_projet, id_chef_de_projet, id_pole) VALUES (505, 'Projet E', '109', 3);
+INSERT INTO admin10.projets (id_projet, nom_projet, id_chef_de_projet, id_pole) VALUES (505, 'Projet E', '109', 3);
 SELECT * FROM admin10.PROJETS;
 
 SELECT * FROM admin10.TACHES;
 UPDATE admin10.taches SET nom_tache = "taches_correction";
-INSERT INTO taches (id_tache, nom_tache, id_responsable_tache, etat_tache, id_projet) VALUES (1005, 'Tâche 5', 104, 'En cours', 505);
+INSERT INTO admin10.taches (id_tache, nom_tache, id_responsable_tache, etat_tache, id_projet) VALUES (1005, 'Tâche 5', 104, 'En cours', 505);
 SELECT * FROM admin10.TACHES;
 DELETE FROM admin10.TACHES WHERE id_tache = 1001;
 DELETE FROM admin10.TACHES WHERE id_tache = 1005;
